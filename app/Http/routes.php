408c1f819bf33b0407b23dd3132bcbe0df848315
kundan <kundan.roy@motifworks.com>
2018-01-23 20:27:06 +0530

    pretige3

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -110,10 +110,11 @@ Route::get('package',[
             ); 
 
 
-Route::get('terns-and-conditions',[
+Route::get('terms-and-conditions',[
           'as' => 'tNc',
           'uses'  => 'HomeController@tNc'
-        ])->name('tNc');
+        ]);
+ 
 
 Route::get('privacy-policy',[
           'as' => 'privacy_and_policy',
